9da61da90401565b24b140c8cacfba6b29c619f0
huangdefang <773256919@qq.com>
2020-12-27 22:27:31 +0800

    111111

---
+++ src/main/java/com/hdf/springboot/controller/TestController.java
@@ -0,0 +1,31 @@
+package com.hdf.springboot.controller;
+
+/**
+ * @author： defang.huang@luckincoffee.com
+ * @date： 2020-12-27 20:47
+ * @description：
+ */
+public class TestController {
+    private void test() {
+        String i = "1111";
+        if ("您好" == i) {
+            System.out.println("lalalalala");
+        }
+        if ("您好" == i) {
+            System.out.println("lalalalala");
+        }
+        if ("您好" == i) {
+            System.out.println("lalalalala");
+        }
+        if ("您好" == i) {
+            System.out.println("lalalalala");
+        }
+        if ("您好" == i) {
+            System.out.println("lalalalala");
+        }
+        if ("您好"==i){
+            System.out.println("lalalalala");
+        }
+    }
+
+}
